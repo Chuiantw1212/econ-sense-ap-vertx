974554e9a5911da9280761bd263e39d590ba84eb
Chuian <chuiantw1212@gmail.com>
2024-07-01 17:02:16 +0800

    add memory

--- src/main/java/com/example/econ_sense_ap_vertx/MainVerticle.java
+++ src/main/java/com/example/econ_sense_ap_vertx/MainVerticle.java
@@ -2,16 +2,25 @@ package com.example.econ_sense_ap_vertx;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServer;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryUsage;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
+    HttpServer server = vertx.createHttpServer();
+
+    server.requestHandler(req -> {
+      String memoryString = this.getMemoryUseInfo();
       req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
+          .putHeader("content-type", "text/plain")
+          .end(memoryString);
+    });
+
+    server.listen(8888, http -> {
       if (http.succeeded()) {
         startPromise.complete();
         System.out.println("HTTP server started on port 8888");
@@ -19,5 +28,18 @@ public class MainVerticle extends AbstractVerticle {
         startPromise.fail(http.cause());
       }
     });
+  }
+
+  public String getMemoryUseInfo() {
+    MemoryUsage mu = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage();
+    long getCommitted = mu.getCommitted();
+    long getInit = mu.getInit();
+    long getUsed = mu.getUsed();
+    long max = mu.getMax();
+
+    return ">>getCommitted(MB)=>" + getCommitted / 1000 / 1000 + "\n"
+        + ">>getInit(MB)=" + getInit / 1000 / 1000 + "\n"
+        + ">>getUsed(MB)=" + getUsed / 1000 / 1000 + "\n"
+        + ">>max(MB)=" + max / 1000 / 1000 + "\n";
   }
 }
